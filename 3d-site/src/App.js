a87a0439b5ee22a4dbe8433c8785f39a860abcdc
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-07-24 16:37:32 +0200

    $0009: [App] [Style]: content: column text positioning

--- 3d-site/src/App.js
+++ 3d-site/src/App.js
@@ -38,12 +38,27 @@ const Content = styled.div`
   position: absolute;
   top: 30px;
 
+  display: flex;
+  flex-direction: column;
+  gap: 80px;
+
   h1 {
     font-family: "Spline Sans Mono", monospace;
     font-weight: bold;
     font-size: 70px;
     margin: 0;
     max-width: 500px;
+  }
+
+  p {
+    font-weight: normal;
+    line-height: 150%;
+    max-width: 300px;
+  }
+
+  h1,
+  p {
+    margin: 0 30px 0 100px;
   }
 `;
 
