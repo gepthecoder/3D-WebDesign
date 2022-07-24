b4a1c0e525c62550e0ff641a5830333f55a8bd70
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-07-24 16:27:06 +0200

    $0007: [App] [Style]: create a wrapper

--- 3d-site/src/App.js
+++ 3d-site/src/App.js
@@ -1,9 +1,29 @@
 import Spline from "@splinetool/react-spline";
+import styled from "styled-components";
 
 function App() {
   return (
-    <Spline scene="https://prod.spline.design/TM-uvVIUVs3hNtQq/scene.splinecode" />
+    <Wrapper>
+      <Spline
+        className="spline"
+        scene="https://prod.spline.design/TM-uvVIUVs3hNtQq/scene.splinecode"
+      />
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  font-family: "Spline Sans", sans-serif;
+  font-size: 16px;
+  color: white;
+  position: relative;
+
+  .spline {
+    position: absolute;
+    margin: 0;
+    top: 0;
+    right: 0;
+  }
+`;
 
 export default App;
