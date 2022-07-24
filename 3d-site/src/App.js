1060cddb1781ffcd7c59a4dd40c14c01f292cb74
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-07-24 17:04:54 +0200

    $0011: [App] [Content]: download button

--- 3d-site/src/App.js
+++ 3d-site/src/App.js
@@ -1,6 +1,11 @@
 import Spline from "@splinetool/react-spline";
 import styled from "styled-components";
 
+import Logo from "./images/logo.svg";
+import Twitter from "./images/icon-twitter.svg";
+import Youtube from "./images/icon-youtube.svg";
+import Laptop from "./images/icon-laptop.svg";
+
 function App() {
   return (
     <Wrapper>
@@ -15,6 +20,10 @@ function App() {
           interpersonal sensitivity and dependability. Here are three ways you
           can start building a foundation for effective teams.
         </p>
+
+        <button>
+          <img src={Laptop} alt="Download"></img> Download For Windows
+        </button>
       </Content>
     </Wrapper>
   );
@@ -53,11 +62,35 @@ const Content = styled.div`
   p {
     font-weight: normal;
     line-height: 150%;
-    max-width: 300px;
+    max-width: 380px;
+  }
+
+  button {
+    background: rgba(0, 0, 0, 0.2);
+    font-size: 16px;
+    padding: 20px 30px;
+    border-radius: 14px;
+    border: 2px solid rgba(255, 255, 255, 0.1);
+    color: white;
+    max-width: 280px;
+    backdrop-filter: blur(20px);
+
+    display: flex;
+    gap: 12px;
+    justify-content: center;
+    align-items: center;
+
+    transition: 1s;
+
+    :hover {
+      border: 1px solid rgba(255, 255, 255, 0.8);
+      transform: translateY(-3px);
+    }
   }
 
   h1,
-  p {
+  p,
+  button {
     margin: 0 30px 0 100px;
   }
 `;
